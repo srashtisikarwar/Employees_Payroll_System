04af8721c5200baaf6e419312a499ff1eaac58b9
Srashti-123 <srashti.sk2000@gmail.com>
2026-05-25 15:08:58 +0530

    Connected Neon PostgreSQL database

--- Employees_Payroll_System/settings.py
+++ Employees_Payroll_System/settings.py
@@ -81,10 +81,9 @@ WSGI_APPLICATION = "Employees_Payroll_System.wsgi.application"
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
 DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
-    }
+    'default': dj_database_url.parse(
+        os.environ.get("DATABASE_URL")
+    )
 }
 
 
