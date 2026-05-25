a149cc550fa09b04dc78ca3ab3dce77709ac541b
Srashti-123 <srashti.sk2000@gmail.com>
2026-05-25 15:43:46 +0530

    Fixed PostgreSQL DATABASE_URL issue

--- Employees_Payroll_System/settings.py
+++ Employees_Payroll_System/settings.py
@@ -82,7 +82,7 @@ WSGI_APPLICATION = "Employees_Payroll_System.wsgi.application"
 
 DATABASES = {
     'default': dj_database_url.parse(
-        os.environ.get("DATABASE_URL")
+        os.environ.get('DATABASE_URL')
     )
 }
 
